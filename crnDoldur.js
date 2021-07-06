57783809b30ad270a82be2370c50459839af5350
letheri <36169207+letheri@users.noreply.github.com>
2021-07-06 17:58:55 +0300

    Create crnDoldur.js

---
+++ crnDoldur.js
@@ -0,0 +1,5 @@
+javascript:let crn=[10000,20000,30000,40000,50000];
+let d=document.getElementsByClassName("form-control");
+console.log(d);
+for(var i=0;i<crn.length;i++){d[i+1].value=crn[i];}
+void(0);
